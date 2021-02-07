28f539226493fa1a5b0e9eea2f7039bd99ca3375
NguyenCaoThin <nguyenthin23102000@gmail.com>
2021-02-07 22:22:48 +0700

    First commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
 app.get('/home', (req, res) => {
     var a = 1
     var b = 2
