fd69ae5129304cae197e0cf4e6a7a2a88672379c
NguyenCaoThin <nguyenthin23102000@gmail.com>
2021-02-23 18:12:35 +0700

    Course detail page and Create new course

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,26 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } =
+require('../../util/mongoose');
 class SiteController {
 
     // [GET] /
-    index(req, res) {
+    index(req, res, next) {
 
-        Course.find({}, function(err, courses) {
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({ error: 'ERROR!!!' });
-            }
-        });
+        // Viết bằng function
+        // Course.find({}, function(err, courses) {
+        //     if (!err) {
+        //         res.json(courses);
+        //     } else {
+        //         next(err);
+        //     }
+        // });
+
+        // Viết bằng promise
+        Course.find({})
+            .then(courses => {
+                res.render('home', { courses: multipleMongooseToObject(courses) })
+            })
+            .catch(next);
 
     }
 
@@ -20,4 +30,4 @@ class SiteController {
     }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+var slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
+    name: { type: String, maxLength: 255, require: true },
     description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
+    image: { type: String },
+    slug: { type: String, slug: 'name', unique: true },
+    videoId: { type: String, maxLength: 255, require: true },
+    level: { type: String, maxLength: 255 },
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
