a1efb68671c1f52944ae3de311b9bcb5e0b30945
NguyenCaoThin <nguyenthin23102000@gmail.com>
2021-02-28 15:19:47 +0700

    Select all with checkbox in stored courses

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,11 +4,16 @@ const { multipleMongooseToObject } = require("../../util/mongoose");
 class MeController {
     // [GET] /me/stored/courses
     storedCourse(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', {
-                courses: multipleMongooseToObject(courses)
-            }))
-            .catch(next)
+
+        Promise.all([Course.find({}), Course.countDocumentsDeleted(), ])
+            .then(([courses, deletedCount]) =>
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses)
+                })
+            )
+            .catch(next);
+
     }
 
     // [GET] /me/trash/courses
