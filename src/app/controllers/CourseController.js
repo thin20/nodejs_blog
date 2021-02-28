e1a2ccc405b777f8f668547a820ede3c56abfb90
NguyenCaoThin <nguyenthin23102000@gmail.com>
2021-02-28 16:50:41 +0700

    Select all with check box in trash courses

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -68,11 +68,29 @@ class CourseController {
             .catch(next);
     }
 
-    // [POST] /courses/handle-form-actions
-    handleFormActions(req, res, next) {
+    // [POST] /courses/handle-form-actions-store-courses
+    handleFormActionsStoreCourses(req, res, next) {
         switch (req.body.action) {
             case 'delete':
                 Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'Action is invalid' })
+        }
+    }
+
+    // [POST] /courses/handle-form-actions-trash-courses
+    handleFormActionsTrashCourses(req, res, next) {
+        switch (req.body.action) {
+            case 'force-destroy':
+                Course.deleteOne({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            case 'restore':
+                Course.restore({ _id: { $in: req.body.courseIds } })
                     .then(() => res.redirect('back'))
                     .catch(next);
                 break;
